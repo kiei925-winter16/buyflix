b90e933d359dc02aae94da8e08be58f819df49a8
Brian Eng <brian@brianeng.com>
2016-01-25 02:57:04 +0000

    week 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # Your routes go here
+  get '/hello', to: 'hello#index'
+  get '/movies', to: 'movies#index'
+  get '/movies/new', to: 'movies#new'
+  post '/movies', to: 'movies#create'
+  get '/movies/:id', to: 'movies#show'
+  get '/movies/:id/edit', to: 'movies#edit'
+  patch '/movies/:id', to: 'movies#update'
+  delete '/movies/:id', to: 'movies#destroy'
 end
